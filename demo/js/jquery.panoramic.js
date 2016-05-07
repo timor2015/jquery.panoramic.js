a0ce06de998f549aebb2311f99a7670e2388934d
duyalei <3209742516@qq.com>
2016-05-07 14:12:18 +0800

    pcDemo大致完成, 不过出现兼容问题

--- demo/js/jquery.panoramic.js
+++ demo/js/jquery.panoramic.js
@@ -4,7 +4,6 @@
 
 	// 设定插件的默认参数设置
 	var defaults = {
-
 		// 图片是否自动转动
 		'autoplay' : false,
 		'auto' : {
@@ -28,6 +27,8 @@
 		'imgLength' : 0,
 		'timer' : null,
 		'timerOff' : true,
+		'float' : null,
+		'img' : null,
 	}
 
 
@@ -36,8 +37,8 @@
 		'panor' : 'position:relative; overflow:hidden;',
 		'float' : 'z-index: 999; width: inherit; height: inherit; position: absolute; cursor: move; left: 0px; top: 0px;',
 		'box' : 'width: inherit; height: inherit; position: absolute; left: 0px; top: 0px;',
-		'img' : 'height: inherit; position: absolute; top: 0px;',
-		'pic' : 'height: inherit; float: left; border: 0px;',
+		'img' : 'height: inherit; position: absolute; top: 0px; left: 0px;',
+		'pic' : 'height: inherit; position: absolute; top: 0px; border: 0px;',
 	}	
 
 
@@ -48,17 +49,25 @@
 			var content = that.find('.panor_img').html();
 			that.find('.panor_img').html(content+content+content);
 			that.attr('style', cssCode.panor);
-			that.find('.panor_float').attr('style', cssCode.float);
+			para.float.attr('style', cssCode.float);
 			that.find('.panor_box').attr('style', cssCode.box);
-			that.find('.panor_img').attr('style', cssCode.img);
+			para.img.attr('style', cssCode.img);			
 			that.find('.panor_img img').attr('style', cssCode.pic);	
 		},
 
-		// 获取图片信息
-		'getImgInfo' : function(that){
-			para.imgLength = that.find('.panor_img img').outerWidth();
-			that.find('.panor_img').width(para.imgLength*3);
-		},
+		// 图片位置重置
+		'resetImgPos' : function(){
+			if (para.nowImgPos > para.imgLength*(-1) && para.nowImgPos < 0) {
+				para.img.css({
+					'left' : para.nowImgPos + para.imgLength*(-1),
+				})
+			}
+			if (para.nowImgPos > para.imgLength*(-3) && para.nowImgPos < para.imgLength*(-2)) {
+				para.img.css({
+					'left' : para.nowImgPos + para.imgLength,
+				})
+			}	
+		}
 	}
 
 
@@ -68,14 +77,95 @@
     	// 设定参数的覆盖顺序()
 		var settings = $.extend( {}, defaults, options);
 		settings.auto = $.extend(defaults.auto, options.auto);
+		para.float = this.find('.panor_float');
+		para.img = this.find('.panor_img');
 		
 
+		
 		// 给标签添加css初始化代码
 		methods.cssInit(this);
 		cssCode = null;
 
+		para.imgLength = parseInt(this.find('.panor_img img').width());
+
 		// 根据图片宽度设定panor_img的宽度
-		methods.getImgInfo(this);
+		para.img.width(para.imgLength*3);
+		para.img.css('left', para.imgLength*(-1));
+
+		// 给图片添加left值
+		this.find('.panor_img img:nth-child(1)').css('left', 0);
+		this.find('.panor_img img:nth-child(2)').css('left', para.imgLength);
+		this.find('.panor_img img:nth-child(3)').css('left', para.imgLength*2);
+
+		// 添加鼠标按下事件
+		para.float.on('mousedown', function(e){
+			e.preventDefault();
+			para.touchOff = true;
+			para.pastPos = e.originalEvent.x || e.originalEvent.layerX || 0;
+		});
+
+		// 鼠标抬起事件
+		$(':root').on('mouseup', function(e){
+			e.preventDefault();
+			para.touchOff = false;
+			methods.resetImgPos();
+		})
+
+		// 移动端事件
+		para.float.on('touchstart', function(e){
+			e.preventDefault();
+			para.touchOff = true;
+			para.pastPos = e.originalEvent.touches[0].pageX || e.originalEvent.changedTouches[0].pageX;
+		});
+
+		$(':root').on('touchend', function(e){
+			e.preventDefault();
+			para.touchOff = false;
+			methods.resetImgPos();
+		});
+
+		// 鼠标移动事件(用户可能会鼠标移出float层, 所有使用委托)
+		$(':root').on('mousemove', function(e){
+			e.preventDefault();
+			if (e.target.className != 'panor_float') {
+				para.touchOff = false;
+				methods.resetImgPos();
+				return;
+			}
+			if (para.touchOff) {
+				para.nowPos = e.originalEvent.x || e.originalEvent.layerX || 0;
+				var iLength = para.nowPos - para.pastPos;
+				if (iLength > 0) {
+					para.dir = 'left'
+				}else{
+					para.dir = 'right'
+				}
+				para.pastPos = para.nowPos;
+				para.nowImgPos = parseInt(para.img.css('left'));
+				para.img.css('left', para.nowImgPos+iLength);
+			}			
+		});
+
+		$(':root').on('touchmove', function(e){
+			e.preventDefault();
+			if (e.target.className != 'panor_float') {
+				para.touchOff = false;
+				methods.resetImgPos();
+				return;
+			}
+			if (para.touchOff) {
+				para.nowPos = e.originalEvent.touches[0].pageX || e.originalEvent.changedTouches[0].pageX;
+				var iLength = para.nowPos - para.pastPos;
+				if (iLength > 0) {
+					para.dir = 'left'
+				}else{
+					para.dir = 'right'
+				}
+				para.pastPos = para.nowPos;
+				para.nowImgPos = parseInt(para.img.css('left'));
+				para.img.css('left', para.nowImgPos+iLength);
+			}			
+		});
 
 
 
